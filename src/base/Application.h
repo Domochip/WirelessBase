0fc458597d5c55de4896b413b4f298242148facd
DomoChip <domochip@hotmail.com>
2024-09-26 21:12:38 +0200

    use using keyword for aliasing WebServer type

--- src/base/Application.h
+++ src/base/Application.h
@@ -5,7 +5,7 @@
 #include <LittleFS.h>
 #ifdef ESP8266
 #include <ESP8266WebServer.h>
-#define WebServer ESP8266WebServer
+using WebServer = ESP8266WebServer;
 #define SERVER_KEEPALIVE_FALSE() server.keepAlive(false);
 #else
 #include <WebServer.h>
