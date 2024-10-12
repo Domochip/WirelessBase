1c71d23b7e88d2866328c816a9f992c568db1d23
DomoChip <domochip@hotmail.com>
2024-10-12 11:29:27 +0200

    Add properties and methods

--- src/base/EventSourceMan.h
+++ src/base/EventSourceMan.h
@@ -4,8 +4,11 @@
 #include "../Main.h"
 #ifdef ESP8266
 #include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+using WebServer = ESP8266WebServer;
 #else
 #include <WiFi.h>
+#include <WebServer.h>
 #endif
 
 #include <Ticker.h>
@@ -13,7 +16,24 @@
 class EventSourceMan
 {
 private:
+#if ENABLE_STATUS_EVTSRC
+    WiFiClient _EventSourceClientList[STATUS_EVTSRC_MAX_CLIENTS];
+#if ENABLE_STATUS_EVTSRC_KEEPALIVE
+    bool _needEventSourceKeepAlive = false;
+    Ticker _eventSourceKeepAliveTicker;
+#endif
+#endif
+
 public:
+    void initEventSourceServer(char appId, WebServer &server);
+
+#if ENABLE_STATUS_EVTSRC
+    void eventSourceHandler(WebServer &server);
+    void eventSourceBroadcast(const String &message, const String &eventType = "message");
+#if ENABLE_STATUS_EVTSRC_KEEPALIVE
+    void eventSourceKeepAlive();
+#endif
+#endif
 };
 
 #endif
