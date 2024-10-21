d1e75431cb7103a1e876ddf0fb50f2d42eff2fc8
DomoChip <domochip@hotmail.com>
2024-10-21 18:35:50 +0200

    Improve main.h

--- src/Main.h
+++ src/Main.h
@@ -3,21 +3,21 @@
 
 #include <Arduino.h>
 
-// DomoChip Informations
-// Configuration Web Pages :
-// http://IP/
+// Helper macros to convert a define to a string
+#define STRINGIFY(x) #x
+#define TOSTRING(x) STRINGIFY(x)
 
-#define APPLICATION1_HEADER "WBase.h"
-#define APPLICATION1_NAME "WBase"
+// ----- Should be modified for your application -----
 #define APPLICATION1_DESC "DomoChip Wireless Base"
 #define APPLICATION1_CLASS WBase
-
 #define VERSION_NUMBER "3.0.0"
 
-#define DEFAULT_AP_SSID "WirelessBase"
-#define DEFAULT_AP_PSK "PasswordBase"
+#define APPLICATION1_NAME TOSTRING(APPLICATION1_CLASS)     // stringified class name
+#define APPLICATION1_HEADER TOSTRING(APPLICATION1_CLASS.h) // calculated header file "{APPLICATION1_NAME}.h"
+#define DEFAULT_AP_SSID APPLICATION1_NAME                  // Default Access Point SSID "{APPLICATION1_NAME}{4 digits of ChipID}"
+#define DEFAULT_AP_PSK APPLICATION1_NAME "Pass"            // Default Access Point Password "{APPLICATION1_NAME}Pass"
 
-// Control EventSource code
+// Control EventSourceMan code (To be used by Application if EventSource server is needed)
 #define EVTSRC_ENABLED 1
 #define EVTSRC_MAX_CLIENTS 2
 #define EVTSRC_KEEPALIVE_ENABLED 1
